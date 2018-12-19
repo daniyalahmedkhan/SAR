acd74e699aff9eab5568eb6ba20587236091daab
daniyalahmed <daniyalahmed520@gmail.com>
2018-12-19 11:56:53 +0500

    From EFU day 2

--- app/src/main/java/shootingrealworldgame/sar/Activites/MainActivity.java
+++ app/src/main/java/shootingrealworldgame/sar/Activites/MainActivity.java
@@ -1,19 +1,25 @@
 package shootingrealworldgame.sar.Activites;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import shootingrealworldgame.sar.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         setContentView(R.layout.activity_main);
-
-
-        Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show();
     }
 }
